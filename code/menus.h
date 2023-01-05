16d1067bb046e3d8d9728438f33858601f6da1e6
S12cybersecurity <79543461+S12cybersecurity@users.noreply.github.com>
2023-01-05 21:42:03 +0100

    Add files via upload

--- code/menus.h
+++ code/menus.h
@@ -13,19 +13,19 @@ int mainmenu() {
 void helpmenu(){
     cyan("\nHELP MENU\n");
     red("Disclaimer: Im not responsable of the use for this tool\n");
-    cyan("\nDeveloped with: C++ - Python - Bash\nVersion 1.1\n\nGithub: https://github.com/S12cybersecurity/RedNeuron\nMedium: https://medium.com/@s12deff\n\nCommands:\n- help -> List this Menu\n- list -> List avaible commands\n- commands -> List avaible commands\n- create -> Section to Create avaible Malware\n- clear -> Clear actual screen\n");
+    cyan("\nDeveloped with: C++ - Python - Bash\nVersion 1.1\n\nGithub: https://github.com/S12cybersecurity/RedNeuron\nMedium: https://medium.com/@s12deff\n\nCommands:\n- help -> List this Menu\n- list -> List available commands\n- commands -> List available commands\n- create -> Section to Create available Malware\n- clear -> Clear actual screen\n");
 }
 
 void commandsmenu(){
-    cyan("\nCommands:\n- help -> List this Menu\n- list -> List avaible commands\n- commands -> List avaible commands\n- create -> Section to Create avaible Malware\n- clear -> Clear actual screen\n");
+    cyan("\nCommands:\n- help -> List this Menu\n- list -> List available commands\n- commands -> List available commands\n- create -> Section to Create available Malware\n- clear -> Clear actual screen\n");
 }
 
 void createcommands(){
-    cyan("\nShell Commands:\n- help -> Show Help Menu\n- list -> List all malware avaible \n- commands -> List avaible commands\n- clear -> Clear actual screen\n\nMalware Commands:\n- reverse shell -> Create Undetectable Windows Reverse Shell\n- revshell -> Create Undetectable Windows Reverse Shell\n");
+    cyan("\nShell Commands:\n- help -> Show Help Menu\n- list -> List all malware available \n- commands -> List available commands\n- clear -> Clear actual screen\n\nMalware Commands:\n- reverse shell -> Create Undetectable Windows Reverse Shell\n- revshell -> Create Undetectable Windows Reverse Shell\n");
 }
 
 void createmenu() {
-    cyan("\nHelp Menu for Create\nIf you want to see all malware avaible execute 'list' command \n\nCreate Section in RedNeuron allow you to use this commands:\n\nShell Commands:\n- help -> Show Help Menu\n- list -> List all malware avaible \n- commands -> List avaible commands\n- clear -> Clear actual screen\n\nMalware Commands:\n- reverse shell -> Create Undetectable Windows Reverse Shell\n- revshell -> Create Undetectable Windows Reverse Shell\n");
+    cyan("\nHelp Menu for Create\nIf you want to see all malware available execute 'list' command \n\nCreate Section in RedNeuron allow you to use this commands:\n\nShell Commands:\n- help -> Show Help Menu\n- list -> List all malware available \n- commands -> List available commands\n- clear -> Clear actual screen\n\nMalware Commands:\n- reverse shell -> Create Undetectable Windows Reverse Shell\n- revshell -> Create Undetectable Windows Reverse Shell\n");
 }
 
 void listcreate(){
